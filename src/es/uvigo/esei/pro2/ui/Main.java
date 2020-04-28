c508f57d337bee419394c5a3c4e611e6037bdef8
Pumita <kikefontanlorenzo@gmail.com>
2020-04-28 22:24:59 +0200

    :fire: Borrados comentarios de prueba

--- src/es/uvigo/esei/pro2/ui/Main.java
+++ src/es/uvigo/esei/pro2/ui/Main.java
@@ -3,17 +3,15 @@
  * Gestión de una clínica. 
  */
 package es.uvigo.esei.pro2.ui;
-//import es.uvigo.esei.pro2.core.*;
+
 /**
  *
  * @author Suraei
  */
 
 public class Main {
-    public static void main(String[] args)
-    {
-//        Persona juan = new Privado("Lopez, Juan", "Chupilandia", "1212<312", new Fecha(14,05,1998), "222222J");
-//        System.out.println("1. " + juan.toString());
+
+    public static void main(String[] args) {
         new Ilc().ler();
     }
 }
